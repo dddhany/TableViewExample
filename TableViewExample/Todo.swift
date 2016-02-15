dff2ef940286db4f70e43ebe94038a0bd5d48066
Dhany Basuki <dddhany+github@gmail.com>
2016-02-15 22:58:33 +0800

    update till video 4

---
+++ TableViewExample/Todo.swift
@@ -0,0 +1,18 @@
+//
+//  Todo.swift
+//  TableViewExample
+//
+//  Created by Dhany Basuki on 15/2/16.
+//  Copyright © 2016 Dhany. All rights reserved.
+//
+
+import UIKit
+
+class Todo {
+    var name: String!
+    var completed: Bool = false
+    
+    init(name: String) {
+        self.name = name
+    }
+}
--- TableViewExample/ViewController.swift
+++ TableViewExample/ViewController.swift
@@ -10,23 +10,28 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var array = [Todo]()
+    
     @IBOutlet weak var tableView: UITableView!
     
     // MARK: Tableview data source methods
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return array.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = "Hello"
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell")!
+        cell.textLabel?.text = array[indexPath.row].name
         return cell
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        array.append(Todo(name: "Eat Breakfast"))
+        array.append(Todo(name: "Brush Teeth"))
+        array.append(Todo(name: "Tie Shoes"))
     }
 
     override func didReceiveMemoryWarning() {
