a9a04b897e88a5f8cb116cd4fa0a44178ff26021
Dhany Basuki <dddhany+github@gmail.com>
2016-02-15 23:11:35 +0800

    update till video 5

---
+++ TableViewExample/AddTodoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddTodoViewController.swift
+//  TableViewExample
+//
+//  Created by Dhany Basuki on 15/2/16.
+//  Copyright © 2016 Dhany. All rights reserved.
+//
+
+import UIKit
+
+class AddTodoViewController: UIViewController {
+
+    @IBAction func doneButtonTapped(sender: UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+}
